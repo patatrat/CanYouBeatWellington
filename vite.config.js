ab15280b08517222954dc8162848ada12c3664f5
patatrat <69439531+patatrat@users.noreply.github.com>
2024-09-03 23:41:25 +1200

    Update vite.config.js
    which tells Vite to use relative paths for all assets

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,25 @@
 import { defineConfig } from 'vite'
+
 import react from '@vitejs/plugin-react'
+
 import path from 'path'
 
-// https://vitejs.dev/config/
+
+
 export default defineConfig({
+
   plugins: [react()],
+
   resolve: {
+
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+
+      '@': path.resolve(__dirname, './src'),
+
     },
+
   },
-  build: {
-    base: process.env.NODE_ENV === 'production' ? '/CanYouBeatWellington/' : '/',
-  },
+
+  base: './',
+
 })
