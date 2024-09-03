e0a20d16d8a5e795104159927f72b13768af6d44
patatrat <69439531+patatrat@users.noreply.github.com>
2024-09-04 09:19:20 +1200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,19 @@
-import { fileURLToPath, URL } from "url";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { resolve } from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: "::",
-    port: "8080",
-  },
-  plugins: [react()],
-  resolve: {
-    alias: [
-      {
-        find: "@",
-        replacement: fileURLToPath(new URL("./src", import.meta.url)),
-      },
-      {
-        find: "lib",
-        replacement: resolve(__dirname, "lib"),
-      },
-    ],
-  },
-});
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      minify: true,
+      inject: {
+        data: {
+          baseUrl: '/canyoubeatwellington/'
+        }
+      }
+    })
+  ],
+  base: '/canyoubeatwellington/'
+})
