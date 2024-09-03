078ce2306efeb85e44bad59fb8a38fab82d89657
patatrat <69439531+patatrat@users.noreply.github.com>
2024-09-04 09:08:02 +1200

    Update vite.config.js
    Base updated to canyoubeatwellington

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,26 @@
-import { fileURLToPath, URL } from "url";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { resolve } from "path";
+import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
+import react from '@vitejs/plugin-react'
+
+import path from 'path'
+
+
+
 export default defineConfig({
-  server: {
-    host: "::",
-    port: "8080",
-  },
+
   plugins: [react()],
+
   resolve: {
-    alias: [
-      {
-        find: "@",
-        replacement: fileURLToPath(new URL("./src", import.meta.url)),
-      },
-      {
-        find: "lib",
-        replacement: resolve(__dirname, "lib"),
-      },
-    ],
+
+    alias: {
+
+      '@': path.resolve(__dirname, './src'),
+
+    },
+
   },
+
+  base: '/CanYouBeatWellington/',
+
+})
 });
